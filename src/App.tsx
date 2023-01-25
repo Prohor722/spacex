c7595cd21f3705475c9a7469c244c0cd952563d9
Prohor722 <prohor.b722@gmail.com>
2023-01-25 20:54:35 +0600

    project initilized

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import Launches from './pages/Launches';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className='bg-orange-200'>Hello there!</p>
+      <Launches />
+      <Button>Conda</Button>
     </div>
   );
 }
